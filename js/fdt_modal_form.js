59cc8074471ea6e373d9b6dbbb7a2783f59292af
Slishkin Anton <slishkinanton@mail.ru>
2021-08-09 14:04:57 +0500

    Show/Hide Modal form element

--- js/fdt_modal_form.js
+++ js/fdt_modal_form.js
@@ -4,10 +4,18 @@
 
   Drupal.behaviors.fdtModalForm = {
     attach: function (context, settings) {
-      $('.pubfile a', context).click(function () {
+      // $('.pubfile a', context).click(function () {
+      $(".footerleft a", context).click(function (e) {
+        e.preventDefault();
         var endpoint = Drupal.url('fdt/openform');
         Drupal.ajax({ url: endpoint }).execute();
       });
+
+      $(".help-us-form-remind", context).on('click', (function (e) {
+        e.preventDefault();
+        console.log('test');
+        $(".help-us-form-remind-wrapper", context).toggleClass('hidden');
+      }));
     }
   }
 
--- src/Controller/FdtmbkkContoller.php
+++ src/Controller/FdtmbkkContoller.php
@@ -8,24 +8,24 @@ use Drupal\Core\Ajax\AlertCommand;
 use Drupal\Core\Ajax\OpenModalDialogCommand;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\webform\Entity\Webform;
 
 class FdtmbkkContoller extends ControllerBase {
 
   public function getContent() {
 
     $webformid = 'help_un_desa';
-
+    $webform = Webform::load($webformid);
+    $title = $webform->label();
     $build['webform'] = [
       '#type' => 'webform',
       '#webform' => $webformid,
     ];
-
     $renderer = \Drupal::service('renderer');
     $rendered = $renderer->renderRoot($build);
 
     $content['#markup'] = $rendered;
     $content['#attached']['library'][] = 'core/drupal.dialog.ajax';
-    $title = 'Help UN DESA - Publication Download Survey';
     $response = new AjaxResponse();
     $response->addCommand(new OpenModalDialogCommand($title, $content, ['width' => '400', 'height' => '400']));
     return $response;
